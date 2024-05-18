a736c6dd5a5291e6cbc8010da57591a283dfbcab
Quentin Quaadgras <email@quentinquaadgras.com>
2024-05-18 16:45:15 +1200

    Update grow.graphics

--- go.mod
+++ go.mod
@@ -1,12 +1,14 @@
 module grow.graphics/eg
 
-go 1.21.0
+go 1.22.0
 
-require grow.graphics/gd v0.0.0-20240512032421-6cab2dce2af5
+toolchain go1.22.3
+
+require grow.graphics/gd v0.0.0-20240518044401-729439c9f538
 
 require (
 	golang.org/x/text v0.15.0 // indirect
 	grow.graphics/uc v0.0.0-20240128001120-57fde712a2f4 // indirect
 	grow.graphics/xy v0.0.0-20240129002912-5a525950a952 // indirect
-	runtime.link v0.0.0-20240510041921-494aa7fd7f63 // indirect
+	runtime.link v0.0.0-20240513040912-fa84cb01d1ec // indirect
 )
--- ui/todolist/todolist.go
+++ ui/todolist/todolist.go
@@ -3,7 +3,6 @@ package main
 import (
 	"grow.graphics/gd"
 	"grow.graphics/gd/gdextension"
-	"runtime.link/mmm"
 )
 
 type TodoList struct {
@@ -20,12 +19,9 @@ func (h *TodoList) Ready(godot gd.Context) {
 }
 
 func (h *TodoList) OnButtonPressed(godot gd.Context) {
-	label := gd.Create(h.List.AsPointer().Pin(), new(gd.Label))
+	label := gd.Create(godot, new(gd.Label))
 	label.SetText(h.Task.GetText(godot))
 	h.List.AsNode().AddChild(label.AsNode(), false, 0)
-	// we have given ownership of the label to Godot, we we need to end the
-	// lifetime within Go (so that it doesn't get freed automatically).
-	mmm.End(label.AsPointer())
 }
 
 func main() {
