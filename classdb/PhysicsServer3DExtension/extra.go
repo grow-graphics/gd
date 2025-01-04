0646681891f53b857828f4a61b084380f843c09b
Quentin Quaadgras <email@quentinquaadgras.com>
2025-01-04 15:08:18 +1300

    Add SceneTree.Add function.

--- classdb/PhysicsServer3DExtension/extra.go
+++ classdb/PhysicsServer3DExtension/extra.go
@@ -2,7 +2,7 @@ package PhysicsServer3DExtension
 
 import gd "graphics.gd/internal"
 
-type RayResult = gd.PhysicsServer2DExtensionRayResult
-type ShapeResult = gd.PhysicsServer2DExtensionShapeResult
-type ShapeRestInfo = gd.PhysicsServer2DExtensionShapeRestInfo
-type MotionResult = gd.PhysicsServer2DExtensionMotionResult
+type RayResult = gd.PhysicsServer3DExtensionRayResult
+type ShapeResult = gd.PhysicsServer3DExtensionShapeResult
+type ShapeRestInfo = gd.PhysicsServer3DExtensionShapeRestInfo
+type MotionResult = gd.PhysicsServer3DExtensionMotionResult
