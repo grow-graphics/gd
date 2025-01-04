cbd105ae949c9d9bd72f06afda4dfa2e3329c71c
Quentin Quaadgras <email@quentinquaadgras.com>
2025-01-04 16:16:51 +1300

    switch hello triangle to use startup.Rendering

--- 2d/hello_triangle/main.go
+++ 2d/hello_triangle/main.go
@@ -1,62 +1,34 @@
 package main
 
 import (
-	"graphics.gd/classdb"
 	"graphics.gd/classdb/DisplayServer"
-	"graphics.gd/classdb/MainLoop"
 	"graphics.gd/classdb/RenderingServer"
 	"graphics.gd/classdb/Resource"
 	"graphics.gd/startup"
 	"graphics.gd/variant/Color"
-	"graphics.gd/variant/Float"
 	"graphics.gd/variant/Rect2"
 	"graphics.gd/variant/Rect2i"
 	"graphics.gd/variant/Vector2"
 )
 
-type HelloTriangle struct {
-	classdb.Extension[HelloTriangle, MainLoop.Instance]
-
-	viewport Resource.ID
+var (
 	closing  bool
-
+	viewport Resource.ID
 	canvas   Resource.ID
 	triangle Resource.ID
-}
+)
 
-func (app *HelloTriangle) Initialize() {
-	app.viewport = RenderingServer.ViewportCreate()
-
-	DisplayServer.WindowSetWindowEventCallback(app.OnWindowInputEvent)
-	DisplayServer.WindowSetRectChangedCallback(app.OnResize)
-	DisplayServer.WindowSetTitle("Hello Triangle")
-
-	size := DisplayServer.WindowGetSize()
-
-	RenderingServer.Advanced().ViewportAttachToScreen(app.viewport, Rect2.New(0, 0, size.X, size.Y), 0)
-	RenderingServer.ViewportSetClearMode(app.viewport, RenderingServer.ViewportClearAlways)
-	RenderingServer.ViewportSetActive(app.viewport, true)
-	RenderingServer.ViewportSetSize(app.viewport, int(size.X), int(size.Y))
-
-	app.canvas = RenderingServer.CanvasCreate()
-	app.triangle = RenderingServer.CanvasItemCreate()
-
-	RenderingServer.CanvasItemSetParent(app.triangle, app.canvas)
-	RenderingServer.ViewportAttachCanvas(app.viewport, app.canvas)
-
-}
-
-func (app *HelloTriangle) OnWindowInputEvent(event DisplayServer.WindowEvent) {
+func onWindowInputEvent(event DisplayServer.WindowEvent) {
 	switch event {
 	case DisplayServer.WindowEventCloseRequest:
-		app.closing = true
+		closing = true
 	}
 }
 
-func (app *HelloTriangle) OnResize(rect Rect2i.PositionSize) {
+func onResize(rect Rect2i.PositionSize) {
 	size := DisplayServer.WindowGetSize()
 
-	RenderingServer.CanvasItemClear(app.triangle)
+	RenderingServer.CanvasItemClear(triangle)
 	var points = []Vector2.XY{
 		Vector2.New(0, 0),
 		Vector2.New(size.X, 0),
@@ -67,24 +39,42 @@ func (app *HelloTriangle) OnResize(rect Rect2i.PositionSize) {
 		{0, 1, 0, 1},
 		{0, 0, 1, 1},
 	}
-	RenderingServer.CanvasItemAddPolygon(app.triangle, points, colors)
-}
-
-func (app *HelloTriangle) Process(dt Float.X) bool {
-	size := DisplayServer.WindowGetSize()
-	RenderingServer.ViewportSetSize(app.viewport, int(size.X), int(size.Y))
-	RenderingServer.Advanced().ViewportAttachToScreen(app.viewport, Rect2.New(0, 0, size.X, size.Y), 0)
-	return app.closing
-}
-
-func (app *HelloTriangle) PhysicsProcess(dt Float.X) bool { return app.closing }
-
-func (app *HelloTriangle) Finalize() {
-	RenderingServer.FreeRid(app.viewport)
-	RenderingServer.FreeRid(app.canvas)
-	RenderingServer.FreeRid(app.triangle)
+	RenderingServer.CanvasItemAddPolygon(triangle, points, colors)
 }
 
 func main() {
-	startup.MainLoop[HelloTriangle]()
+	frames := startup.Rendering()
+
+	viewport = RenderingServer.ViewportCreate()
+	defer RenderingServer.FreeRid(viewport)
+
+	DisplayServer.WindowSetWindowEventCallback(onWindowInputEvent)
+	DisplayServer.WindowSetRectChangedCallback(onResize)
+	DisplayServer.WindowSetTitle("Hello Triangle")
+
+	size := DisplayServer.WindowGetSize()
+
+	RenderingServer.Advanced().ViewportAttachToScreen(viewport, Rect2.New(0, 0, size.X, size.Y), 0)
+	RenderingServer.ViewportSetClearMode(viewport, RenderingServer.ViewportClearAlways)
+	RenderingServer.ViewportSetActive(viewport, true)
+	RenderingServer.ViewportSetSize(viewport, int(size.X), int(size.Y))
+
+	canvas = RenderingServer.CanvasCreate()
+	triangle = RenderingServer.CanvasItemCreate()
+	defer RenderingServer.FreeRid(canvas)
+	defer RenderingServer.FreeRid(triangle)
+
+	RenderingServer.CanvasItemSetParent(triangle, canvas)
+	RenderingServer.ViewportAttachCanvas(viewport, canvas)
+
+	onResize(Rect2i.New(0, 0, size.X, size.Y))
+
+	for range frames {
+		size := DisplayServer.WindowGetSize()
+		RenderingServer.ViewportSetSize(viewport, int(size.X), int(size.Y))
+		RenderingServer.Advanced().ViewportAttachToScreen(viewport, Rect2.New(0, 0, size.X, size.Y), 0)
+		if closing {
+			break
+		}
+	}
 }
