fa45698867a0a27c42bb79775a5d1ae1c6cc558b
Quentin Quaadgras <email@quentinquaadgras.com>
2025-04-05 12:27:58 +1300

    Add Object.Set and Object.Get

---
+++ variant/Object/global.go
@@ -0,0 +1,25 @@
+package Object
+
+import gd "graphics.gd/internal"
+
+// Any object.
+type Any interface {
+	gd.IsClass
+	AsObject() [1]gd.Object
+}
+
+// Set assigns value to the given property. If the property does not exist or the given
+// value's type doesn't match, nothing happens.
+//
+// Note: property must be in snake_case when referring to built-in Godot properties.
+func Set(object Any, property string, value any) { //gd:Object.set
+	object.AsObject()[0].Set(gd.NewStringName(property), gd.NewVariant(value))
+}
+
+// Get returns the Variant value of the given property. If the property does not exist,
+// this method returns null.
+//
+// Note: property must be in snake_case when referring to built-in Godot properties.
+func Get(object Any, property string) any { //gd:Object.get
+	return object.AsObject()[0].Get(gd.NewStringName(property))
+}
