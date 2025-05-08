843f60e208803e16e3de811d5cdd41b3242a9ae8
Quentin Quaadgras <email@quentinquaadgras.com>
2025-05-08 22:24:43 +1200

    update to latest shaders API

--- 2d/shader/my_first_canvasitem_shader.go
+++ 2d/shader/my_first_canvasitem_shader.go
@@ -1,56 +1,56 @@
 package main
 
 import (
-	"graphics.gd/shaders"
 	"graphics.gd/shaders/bool"
 	"graphics.gd/shaders/float"
+	"graphics.gd/shaders/pipeline/CanvasItem"
 	"graphics.gd/shaders/rgba"
 	"graphics.gd/shaders/vec2"
 )
 
 type MyFirstShader struct {
-	shaders.Type2D
+	CanvasItem.Shader
 
 	Offset vec2.XY `gd:"offset"`
 }
 
-func (MyFirstShader) Material(vert shaders.Fragment2D) shaders.Material2D {
+func (MyFirstShader) Material(vert CanvasItem.Fragment) CanvasItem.Material {
 	// smooth back and forth between pink and blue
-	step := float.Mod(vert.Time, 2.0)
+	step := float.Mod(CanvasItem.Time, 2.0)
 	step = bool.Mix(step, float.Sub(2.0, step), float.Gt(step, 1.0))
-	return shaders.Material2D{
+	return CanvasItem.Material{
 		Color: rgba.New(step, 0.6, 0.9, 1.0),
 	}
 }
 
 type MyFirstShader2D struct {
-	shaders.Type2D
+	CanvasItem.Shader
 }
 
-func (uniform MyFirstShader2D) Material(vertex shaders.Fragment2D) shaders.Material2D {
-	return shaders.Material2D{
+func (uniform MyFirstShader2D) Material(vertex CanvasItem.Fragment) CanvasItem.Material {
+	return CanvasItem.Material{
 		Color: rgba.New(0.4, 0.6, 0.9, 1.0),
 	}
 }
 
 type MyFirstShader2D_UV struct {
-	shaders.Type2D
+	CanvasItem.Shader
 }
 
-func (uniform MyFirstShader2D_UV) Material(fragment shaders.Fragment2D) shaders.Material2D {
-	return shaders.Material2D{
+func (uniform MyFirstShader2D_UV) Material(fragment CanvasItem.Fragment) CanvasItem.Material {
+	return CanvasItem.Material{
 		Color: rgba.New(fragment.UV.X, fragment.UV.Y, 0.5, 1.0),
 	}
 }
 
 type MyFirstShader2D_Texture struct {
-	shaders.Type2D
+	CanvasItem.Shader
 }
 
-func (uniform MyFirstShader2D_Texture) Material(fragment shaders.Fragment2D) shaders.Material2D {
-	color := fragment.Texture.Texture(fragment.UV)
+func (uniform MyFirstShader2D_Texture) Material(fragment CanvasItem.Fragment) CanvasItem.Material {
+	color := fragment.Texture().Texture(fragment.UV)
 	color.B = float.New(1.0)
-	return shaders.Material2D{
+	return CanvasItem.Material{
 		Color: color,
 	}
 }
