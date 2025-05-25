52540fdc689f1bfa15a962ab7123e8fcd6ba7bbe
Quentin Quaadgras <email@quentinquaadgras.com>
2025-05-25 18:44:50 +1200

    remove obselete functions

--- internal/docgen/process.go
+++ internal/docgen/process.go
@@ -1,7 +1,9 @@
 package docgen
 
 import (
+	"encoding/xml"
 	"fmt"
+	"os"
 	"path/filepath"
 )
 
@@ -13,6 +15,20 @@ func Process(packagePath string) error {
 	err = serializeDocs(pkgsDocs, filepath.Join(packagePath, "graphics", "library_documentation.xml"))
 	if err != nil {
 		return fmt.Errorf("failed to serialize documentation: %w", err)
+	}
+	return nil
+}
+
+func serializeDocs(docs XML, outPath string) error {
+	file, err := os.Create(outPath)
+	if err != nil {
+		return fmt.Errorf("failed to create doc gob file: %w", err)
+	}
+	defer file.Close()
+	enc := xml.NewEncoder(file)
+	enc.Indent("", "\t")
+	if err := enc.Encode(docs); err != nil {
+		return fmt.Errorf("failed to encode docs: %w", err)
 	}
 	return nil
 }
