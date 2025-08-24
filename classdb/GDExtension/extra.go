97c263ee5efcde50ed5b418829827e1821b21bd8
Quentin Quaadgras <email@quentinquaadgras.com>
2025-08-25 00:52:26 +1200

    fix GDExtension.GetLibraryPath

--- classdb/GDExtension/extra.go
+++ classdb/GDExtension/extra.go
@@ -1,8 +1,12 @@
 package GDExtension
 
-import gd "graphics.gd/internal"
+import (
+	gd "graphics.gd/internal"
+	"graphics.gd/internal/gdextension"
+	"graphics.gd/internal/pointers"
+)
 
 // LibraryPath is the path to the shared library that contains the current GD extension.
 func LibraryPath() string {
-	return gd.Global.GetLibraryPath(gd.Global.ExtensionToken).String()
+	return pointers.New[gd.String](gdextension.Host.Library.Location()).String()
 }
