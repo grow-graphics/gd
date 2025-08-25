2163bdfbab72e1b6aae8deb8dd4c2fbfd1315c42
Quentin Quaadgras <email@quentinquaadgras.com>
2025-08-25 22:20:08 +1200

    add Get to variant.Arguments

--- variant/args.go
+++ variant/args.go
@@ -2,3 +2,8 @@ package variant
 
 // Arguments efficiently represents a variable number of arguments, for use in trampolines.
 type Arguments struct{}
+
+// Get the nth argument from the arguments list.
+func (args Arguments) Get(index int) Any {
+	return Any{}
+}
